0b8a26ce5a9437fd6bd266f8db4bd16e04df7a1f
Gary Paul Smith <gapsm@eg.dk>
2018-04-20 12:53:46 +0200

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,10 +9,35 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+/*
+    router.post(InfoData.self, at: "info") { req, data -> String in
+        
+        // let data = try req.content.decode(InfoData.self).await(on: req)
+        return "Hello \(data.name)!"
+    }
+*/
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        
+        // let data = try req.content.decode(InfoData.self).await(on: req)
+        return InfoResponse(request: data)
+    }
+    
+
+    
+    
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
+}
+
+struct InfoData: Content {
+    let name: String
 }
